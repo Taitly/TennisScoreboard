0f43fe8479a876b7aaef23699eb367e0df163c00
Taitly <kilroynik@gmail.com>
2025-06-04 10:27:22 +0300

    feat: add Hibernate model classes for database tables

---
+++ src/main/java/com/taitly/tennisscoreboard/entity/Match.java
@@ -0,0 +1,29 @@
+package com.taitly.tennisscoreboard.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "matches")
+public class Match {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "player1")
+    private Player player1;
+
+    @ManyToOne
+    @JoinColumn(name = "player2")
+    private Player player2;
+
+    @ManyToOne
+    @JoinColumn(name = "winner")
+    private Player winner;
+}
---
+++ src/main/java/com/taitly/tennisscoreboard/entity/Player.java
@@ -0,0 +1,20 @@
+package com.taitly.tennisscoreboard.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "players")
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String name;
+}
