ed297328e92c3c216a02d3c32a1900aaed3ddd20
Taitly <kilroynik@gmail.com>
2025-06-04 10:31:11 +0300

    feat: add SQL script for initial database data

---
+++ src/main/resources/init.sql
@@ -0,0 +1,11 @@
+INSERT INTO players (name) values ('Ethan Caldwell');
+INSERT INTO players (name) values ('Olivia Bennett');
+INSERT INTO players (name) values ('Lucas Moretti');
+INSERT INTO players (name) values ('Sofia Navarro');
+INSERT INTO players (name) values ('Daniel Whitaker');
+
+INSERT INTO matches (player1, player2, winner) values ('2', '5', '2');
+INSERT INTO matches (player1, player2, winner) values ('1', '3', '3');
+INSERT INTO matches (player1, player2, winner) values ('1', '2', '1');
+INSERT INTO matches (player1, player2, winner) values ('5', '4', '5');
+INSERT INTO matches (player1, player2, winner) values ('4', '3', '4');
