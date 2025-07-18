687831da2afbf715fa61d3895639ca2ff73d3b1b
Taitly <kilroynik@gmail.com>
2025-07-18 16:51:30 +0300

    feat: add MapStruct mapper for MatchScore and MatchScoreDto

---
+++ src/main/java/com/taitly/tennisscoreboard/mapper/MatchScoreMapper.java
@@ -0,0 +1,15 @@
+package com.taitly.tennisscoreboard.mapper;
+
+import com.taitly.tennisscoreboard.dto.MatchScoreDto;
+import com.taitly.tennisscoreboard.model.MatchScore;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface MatchScoreMapper {
+    MatchScoreMapper INSTANCE = Mappers.getMapper(MatchScoreMapper.class);
+
+    MatchScoreDto toDto(MatchScore matchScore);
+
+    MatchScore toEntity(MatchScoreDto matchScoreDto);
+}
