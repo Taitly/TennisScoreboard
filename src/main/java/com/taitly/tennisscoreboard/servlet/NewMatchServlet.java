d56c3a12e337a1896e93514dcd113b1a3bcebbfe
Taitly <kilroynik@gmail.com>
2025-07-21 12:47:06 +0300

    feat: validate player names and ensure distinctness before creating new match in NewMatchServlet

--- src/main/java/com/taitly/tennisscoreboard/servlet/NewMatchServlet.java
+++ src/main/java/com/taitly/tennisscoreboard/servlet/NewMatchServlet.java
@@ -4,6 +4,7 @@ import com.taitly.tennisscoreboard.dto.MatchScoreDto;
 import com.taitly.tennisscoreboard.entity.Player;
 import com.taitly.tennisscoreboard.model.TennisPoint;
 import com.taitly.tennisscoreboard.service.OngoingMatchesService;
+import com.taitly.tennisscoreboard.validation.PlayerValidator;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,7 +16,6 @@ import java.util.UUID;
 
 @WebServlet("/new-match")
 public class NewMatchServlet extends HttpServlet {
-
     private final OngoingMatchesService ongoingMatchesService = OngoingMatchesService.getINSTANCE();
 
     @Override
@@ -24,9 +24,13 @@ public class NewMatchServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String firstPlayerName = req.getParameter("playerOne").trim();
         String secondPlayerName = req.getParameter("playerTwo").trim();
+
+        PlayerValidator.validatePlayerName(firstPlayerName);
+        PlayerValidator.validatePlayerName(secondPlayerName);
+        PlayerValidator.validatePlayersNamesAreDifferent(firstPlayerName, secondPlayerName);
 
         Player firstPlayer = Player.builder()
                 .name(firstPlayerName)
@@ -36,14 +40,10 @@ public class NewMatchServlet extends HttpServlet {
                 .name(secondPlayerName)
                 .build();
 
-        MatchScoreDto matchScore = MatchScoreDto.builder()
-                .firstPlayer(firstPlayer)
-                .secondPlayer(secondPlayer)
-                .firstPlayerPoint(TennisPoint.ZERO)
-                .secondPlayerPoint(TennisPoint.ZERO)
-                .build();
+        MatchScoreDto matchScoreDto = new MatchScoreDto(firstPlayer, secondPlayer, 0, 0,
+                0, 0, TennisPoint.ZERO, TennisPoint.ZERO, 0, 0);
 
-        UUID uuid = ongoingMatchesService.setMatchScore(matchScore);
+        UUID uuid = ongoingMatchesService.setMatchScore(matchScoreDto);
 
         resp.sendRedirect(req.getContextPath() + "/match-score?uuid=" + uuid);
     }
