6257ab9db0ec61fbcf40f6d520f8eee3b05137f8
Taitly <kilroynik@gmail.com>
2025-07-21 12:43:38 +0300

    refactor: use MatchScoreMapper to map between DTO and entity in MatchScoreCalculationService

--- src/main/java/com/taitly/tennisscoreboard/service/MatchScoreCalculationService.java
+++ src/main/java/com/taitly/tennisscoreboard/service/MatchScoreCalculationService.java
@@ -2,30 +2,36 @@ package com.taitly.tennisscoreboard.service;
 
 import com.taitly.tennisscoreboard.dto.MatchScoreDto;
 import com.taitly.tennisscoreboard.entity.Player;
+import com.taitly.tennisscoreboard.mapper.MatchScoreMapper;
+import com.taitly.tennisscoreboard.model.MatchScore;
 import com.taitly.tennisscoreboard.model.TennisPoint;
 import lombok.Getter;
 
 public class MatchScoreCalculationService {
-
     @Getter
     private static final MatchScoreCalculationService INSTANCE = new MatchScoreCalculationService();
 
+    private final MatchScoreMapper matchScoreMapper = MatchScoreMapper.INSTANCE;
     private static final int SETS_TO_WIN = 2;
     private static final int MINIMAL_GAMES_TO_WIN = 6;
     private static final int TIE_BREAK_POINTS_TO_WIN = 7;
 
-    public void playerScores(MatchScoreDto matchScore, Player player) {
+    private MatchScoreCalculationService() {
+
+    }
+
+    public MatchScoreDto playerScores(MatchScoreDto matchScoreDto, Player player) {
+        MatchScore matchScore = matchScoreMapper.toEntity(matchScoreDto);
+
         if (isTieBreak(matchScore)) {
 
             updateTieBreakPoints(matchScore, player);
 
             if (isTieBreakFinished(matchScore)) {
-
                 updateSets(matchScore, player);
                 resetGames(matchScore);
                 resetTieBreakPoints(matchScore);
                 resetPoints(matchScore);
-
             }
         } else {
 
@@ -38,11 +44,11 @@ public class MatchScoreCalculationService {
 
             if (oldPlayerPoint == TennisPoint.FORTY && oldOpponentPoint == TennisPoint.FORTY &&
                     newPlayerPoint == TennisPoint.ADVANTAGE) {
-                return;
+                return matchScoreMapper.toDto(matchScore);
             }
 
             if (oldPlayerPoint == TennisPoint.THIRTY && newPlayerPoint == TennisPoint.FORTY) {
-                return;
+                return matchScoreMapper.toDto(matchScore);
             }
 
             if (isGameWon(matchScore, player)) {
@@ -58,21 +64,24 @@ public class MatchScoreCalculationService {
                 }
             }
         }
+        return matchScoreMapper.toDto(matchScore);
     }
 
-    public Player getWinner(MatchScoreDto matchScore) {
+    public Player getWinner(MatchScoreDto matchScoreDto) {
+        MatchScore matchScore = matchScoreMapper.toEntity(matchScoreDto);
         if (matchScore.getFirstPlayerSets() == SETS_TO_WIN) {
             return matchScore.getFirstPlayer();
         }
         return matchScore.getSecondPlayer();
     }
 
-    public boolean isMatchOver(MatchScoreDto matchScore) {
+    public boolean isMatchOver(MatchScoreDto matchScoreDto) {
+        MatchScore matchScore = matchScoreMapper.toEntity(matchScoreDto);
         return matchScore.getFirstPlayerSets() == SETS_TO_WIN ||
                 matchScore.getSecondPlayerSets() == SETS_TO_WIN;
     }
 
-    private void updateNormalPoints(MatchScoreDto matchScore, Player player) {
+    private void updateNormalPoints(MatchScore matchScore, Player player) {
         boolean isFirstPlayer = player.equals(matchScore.getFirstPlayer());
 
         TennisPoint firstPlayerPoint = matchScore.getFirstPlayerPoint();
@@ -102,7 +111,7 @@ public class MatchScoreCalculationService {
         return current.next();
     }
 
-    private void updateTieBreakPoints(MatchScoreDto matchScore, Player player) {
+    private void updateTieBreakPoints(MatchScore matchScore, Player player) {
         int firstPlayerTieBreakPoints = matchScore.getFirstPlayerTieBreakPoints();
         int secondPlayerTieBreakPoints = matchScore.getSecondPlayerTieBreakPoints();
 
@@ -113,7 +122,7 @@ public class MatchScoreCalculationService {
         }
     }
 
-    private void updateGames(MatchScoreDto matchScore, Player player) {
+    private void updateGames(MatchScore matchScore, Player player) {
         int firstPlayerGames = matchScore.getFirstPlayerGames();
         int secondPlayerGames = matchScore.getSecondPlayerGames();
 
@@ -124,7 +133,7 @@ public class MatchScoreCalculationService {
         }
     }
 
-    private void updateSets(MatchScoreDto matchScore, Player player) {
+    private void updateSets(MatchScore matchScore, Player player) {
         int firstPlayerSets = matchScore.getFirstPlayerSets();
         int secondPlayerSets = matchScore.getSecondPlayerSets();
 
@@ -135,7 +144,7 @@ public class MatchScoreCalculationService {
         }
     }
 
-    private void handleDeuce(MatchScoreDto matchScore, boolean isFirstPlayer) {
+    private void handleDeuce(MatchScore matchScore, boolean isFirstPlayer) {
         if (isFirstPlayer) {
             matchScore.setFirstPlayerPoint(TennisPoint.ADVANTAGE);
             matchScore.setSecondPlayerPoint(TennisPoint.FORTY);
@@ -145,7 +154,7 @@ public class MatchScoreCalculationService {
         }
     }
 
-    private void handleAdvantage(MatchScoreDto matchScore, boolean isFirstPlayer) {
+    private void handleAdvantage(MatchScore matchScore, boolean isFirstPlayer) {
         if (isFirstPlayer) {
             if (matchScore.getFirstPlayerPoint() == TennisPoint.ADVANTAGE) {
                 resetPoints(matchScore);
@@ -165,22 +174,22 @@ public class MatchScoreCalculationService {
         }
     }
 
-    private void resetPoints(MatchScoreDto matchScore) {
+    private void resetPoints(MatchScore matchScore) {
         matchScore.setFirstPlayerPoint(TennisPoint.ZERO);
         matchScore.setSecondPlayerPoint(TennisPoint.ZERO);
     }
 
-    private void resetGames(MatchScoreDto matchScore) {
+    private void resetGames(MatchScore matchScore) {
         matchScore.setFirstPlayerGames(0);
         matchScore.setSecondPlayerGames(0);
     }
 
-    private void resetTieBreakPoints(MatchScoreDto matchScore) {
+    private void resetTieBreakPoints(MatchScore matchScore) {
         matchScore.setFirstPlayerTieBreakPoints(0);
         matchScore.setSecondPlayerTieBreakPoints(0);
     }
 
-    private boolean isGameWon(MatchScoreDto matchScore, Player player) {
+    private boolean isGameWon(MatchScore matchScore, Player player) {
         TennisPoint playerPoint = getPlayerPoint(matchScore, player);
         TennisPoint opponentPoint = getOpponentPoint(matchScore, player);
 
@@ -195,7 +204,7 @@ public class MatchScoreCalculationService {
         return playerPoint == TennisPoint.FORTY && opponentPoint.ordinal() < TennisPoint.FORTY.ordinal();
     }
 
-    private boolean isSetWon(MatchScoreDto matchScore) {
+    private boolean isSetWon(MatchScore matchScore) {
         int firstPlayerGames = matchScore.getFirstPlayerGames();
         int secondPlayerGames = matchScore.getSecondPlayerGames();
 
@@ -207,14 +216,14 @@ public class MatchScoreCalculationService {
                 (secondPlayerGames >= MINIMAL_GAMES_TO_WIN && secondPlayerGames - firstPlayerGames >= 2);
     }
 
-    private boolean isTieBreakFinished(MatchScoreDto matchScore) {
+    private boolean isTieBreakFinished(MatchScore matchScore) {
         int firstPlayerTieBreakPoints = matchScore.getFirstPlayerTieBreakPoints();
         int secondPlayerTieBreakPoints = matchScore.getSecondPlayerTieBreakPoints();
         return (firstPlayerTieBreakPoints >= TIE_BREAK_POINTS_TO_WIN && firstPlayerTieBreakPoints - secondPlayerTieBreakPoints >= 2) ||
                 (secondPlayerTieBreakPoints >= TIE_BREAK_POINTS_TO_WIN && secondPlayerTieBreakPoints - firstPlayerTieBreakPoints >= 2);
     }
 
-    private boolean isTieBreak(MatchScoreDto matchScore) {
+    private boolean isTieBreak(MatchScore matchScore) {
         return isTieBreak(matchScore.getFirstPlayerGames(), matchScore.getSecondPlayerGames());
     }
 
@@ -230,7 +239,7 @@ public class MatchScoreCalculationService {
         return firstPlayerPoint == TennisPoint.ADVANTAGE || secondPlayerPoint == TennisPoint.ADVANTAGE;
     }
 
-    private TennisPoint getPlayerPoint(MatchScoreDto matchScore, Player player) {
+    private TennisPoint getPlayerPoint(MatchScore matchScore, Player player) {
         if (player.equals(matchScore.getFirstPlayer())) {
             return matchScore.getFirstPlayerPoint();
         } else {
@@ -238,15 +247,11 @@ public class MatchScoreCalculationService {
         }
     }
 
-    private TennisPoint getOpponentPoint(MatchScoreDto matchScore, Player player) {
+    private TennisPoint getOpponentPoint(MatchScore matchScore, Player player) {
         if (player.equals(matchScore.getFirstPlayer())) {
             return matchScore.getSecondPlayerPoint();
         } else {
             return matchScore.getFirstPlayerPoint();
         }
-    }
-
-    private MatchScoreCalculationService() {
-
     }
 }
