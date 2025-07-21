1134616da4cd8018cbe10cd5b2c92f3c006fd191
Taitly <kilroynik@gmail.com>
2025-07-21 12:45:38 +0300

    refactor: switch to entity-based match storage with DTO mapping and update method in OngoingMatchesService

--- src/main/java/com/taitly/tennisscoreboard/service/OngoingMatchesService.java
+++ src/main/java/com/taitly/tennisscoreboard/service/OngoingMatchesService.java
@@ -1,6 +1,8 @@
 package com.taitly.tennisscoreboard.service;
 
 import com.taitly.tennisscoreboard.dto.MatchScoreDto;
+import com.taitly.tennisscoreboard.mapper.MatchScoreMapper;
+import com.taitly.tennisscoreboard.model.MatchScore;
 import lombok.Getter;
 
 import java.util.Map;
@@ -8,27 +10,34 @@ import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class OngoingMatchesService {
-    private final static Map<UUID, MatchScoreDto> currentMatches = new ConcurrentHashMap<>();
-
     @Getter
     private static final OngoingMatchesService INSTANCE = new OngoingMatchesService();
 
+    private final MatchScoreMapper matchScoreMapper = MatchScoreMapper.INSTANCE;
+    private final static Map<UUID, MatchScore> currentMatches = new ConcurrentHashMap<>();
+
+    private OngoingMatchesService() {
+
+    }
+
     public MatchScoreDto getMatchScore(UUID uuid) {
-        return currentMatches.get(uuid);
+        MatchScore matchScore = currentMatches.get(uuid);
+        return matchScoreMapper.toDto(matchScore);
     }
 
     public UUID setMatchScore(MatchScoreDto matchScoreDto) {
         UUID uuid = UUID.randomUUID();
-
-        currentMatches.put(uuid, matchScoreDto);
+        MatchScore matchScore = matchScoreMapper.toEntity(matchScoreDto);
+        currentMatches.put(uuid, matchScore);
         return uuid;
+    }
+
+    public void updateMatchScore(UUID uuid, MatchScoreDto matchScoreDto) {
+        MatchScore matchScore = MatchScoreMapper.INSTANCE.toEntity(matchScoreDto);
+        currentMatches.put(uuid, matchScore);
     }
 
     public void removeMatchScore(UUID uuid) {
         currentMatches.remove(uuid);
-    }
-
-    private OngoingMatchesService() {
-
     }
 }
