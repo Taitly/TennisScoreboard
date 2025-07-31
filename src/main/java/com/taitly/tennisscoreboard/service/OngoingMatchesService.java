063ca99d0f7c23c18fcd4c7f8cabbbf0d2d8e40e
Taitly <kilroynik@gmail.com>
2025-07-31 11:03:24 +0300

    refactor: update OngoingMatchesService to manage MatchScore entities and delegate scoring updates

--- src/main/java/com/taitly/tennisscoreboard/service/OngoingMatchesService.java
+++ src/main/java/com/taitly/tennisscoreboard/service/OngoingMatchesService.java
@@ -1,8 +1,9 @@
 package com.taitly.tennisscoreboard.service;
 
-import com.taitly.tennisscoreboard.dto.MatchScoreDto;
-import com.taitly.tennisscoreboard.mapper.MatchScoreMapper;
+import com.taitly.tennisscoreboard.entity.Player;
+import com.taitly.tennisscoreboard.exception.MatchNotFoundException;
 import com.taitly.tennisscoreboard.model.MatchScore;
+import com.taitly.tennisscoreboard.model.TennisSet;
 import lombok.Getter;
 
 import java.util.Map;
@@ -13,28 +14,39 @@ public class OngoingMatchesService {
     @Getter
     private static final OngoingMatchesService INSTANCE = new OngoingMatchesService();
 
-    private final MatchScoreMapper matchScoreMapper = MatchScoreMapper.INSTANCE;
+    private final MatchScoreCalculationService matchScoreCalculationService = MatchScoreCalculationService.getINSTANCE();
     private final static Map<UUID, MatchScore> currentMatches = new ConcurrentHashMap<>();
 
     private OngoingMatchesService() {
 
     }
 
-    public MatchScoreDto getMatchScore(UUID uuid) {
+    public MatchScore getMatchScore(UUID uuid) {
         MatchScore matchScore = currentMatches.get(uuid);
-        return matchScoreMapper.toDto(matchScore);
+        if (matchScore == null) {
+            throw new MatchNotFoundException("Match not found");
+        }
+        return matchScore;
     }
 
-    public UUID setMatchScore(MatchScoreDto matchScoreDto) {
+    public UUID setMatchScore(MatchScore matchScore) {
         UUID uuid = UUID.randomUUID();
-        MatchScore matchScore = matchScoreMapper.toEntity(matchScoreDto);
+
+        Player firstPlayer = matchScore.getFirstPlayerScore().getPlayer();
+        Player secondPlayer = matchScore.getSecondPlayerScore().getPlayer();
+
+        matchScore.getSets().add(new TennisSet(firstPlayer, secondPlayer));
+
         currentMatches.put(uuid, matchScore);
         return uuid;
     }
 
-    public void updateMatchScore(UUID uuid, MatchScoreDto matchScoreDto) {
-        MatchScore matchScore = MatchScoreMapper.INSTANCE.toEntity(matchScoreDto);
-        currentMatches.put(uuid, matchScore);
+    public void updateMatchScore(UUID uuid, Player player) {
+        MatchScore matchScore = currentMatches.get(uuid);
+        if (matchScore == null) {
+            throw new MatchNotFoundException("Match not found");
+        }
+        matchScoreCalculationService.playerScores(matchScore, player);
     }
 
     public void removeMatchScore(UUID uuid) {
