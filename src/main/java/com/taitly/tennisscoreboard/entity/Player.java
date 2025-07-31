423edac98c2d66fe79f558d0562b732be5e620b2
Taitly <kilroynik@gmail.com>
2025-07-31 10:59:52 +0300

    feat: add transient tempId to Player entity for identity before persistence and update equals/hashCode

--- src/main/java/com/taitly/tennisscoreboard/entity/Player.java
+++ src/main/java/com/taitly/tennisscoreboard/entity/Player.java
@@ -3,12 +3,13 @@ package com.taitly.tennisscoreboard.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@EqualsAndHashCode
 @Entity
 @Table(name = "players")
 public class Player {
@@ -18,4 +19,31 @@ public class Player {
 
     @Column(unique = true, nullable = false, length = 50)
     private String name;
+
+    @Transient
+    private final UUID tempId = UUID.randomUUID();
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Player player = (Player) o;
+
+        if (id != null && player.id != null) {
+            return id.equals(player.id);
+        }
+
+        return tempId.equals(player.tempId);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : tempId.hashCode();
+    }
 }
