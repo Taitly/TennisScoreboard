990c6e1d022066ce2bd6b703c4fc5ceecbd095c2
Taitly <kilroynik@gmail.com>
2025-07-31 11:01:36 +0300

    refactor: update MatchValidator to validate existence using MatchScore model instead of DTO

--- src/main/java/com/taitly/tennisscoreboard/validation/MatchValidator.java
+++ src/main/java/com/taitly/tennisscoreboard/validation/MatchValidator.java
@@ -1,7 +1,7 @@
 package com.taitly.tennisscoreboard.validation;
 
-import com.taitly.tennisscoreboard.dto.MatchScoreDto;
 import com.taitly.tennisscoreboard.exception.MatchNotFoundException;
+import com.taitly.tennisscoreboard.model.MatchScore;
 import com.taitly.tennisscoreboard.service.OngoingMatchesService;
 import lombok.experimental.UtilityClass;
 
@@ -12,9 +12,9 @@ public class MatchValidator {
     private final OngoingMatchesService ongoingMatchesService = OngoingMatchesService.getINSTANCE();
 
     public void validateMatchExists(UUID matchId) {
-        MatchScoreDto matchScoreDto = ongoingMatchesService.getMatchScore(matchId);
+        MatchScore matchScore = ongoingMatchesService.getMatchScore(matchId);
 
-        if (matchScoreDto == null) {
+        if (matchScore == null) {
             throw new MatchNotFoundException("Match not found");
         }
     }
